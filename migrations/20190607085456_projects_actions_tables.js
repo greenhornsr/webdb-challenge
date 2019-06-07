22374dfeb86aa4d3fdc69e4d92c5463feb38855e
greenhornsr <srollins11279@gmail.com>
2019-06-07 09:12:29 -0700

    added 2 tables, projects & actions via 1 migration

---
+++ migrations/20190607085456_projects_actions_tables.js
@@ -0,0 +1,53 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('projects', function(table) {
+        table.increments()
+
+        table
+        .string('name', 128)
+        .unique()
+        .notNullable()
+
+        table
+        .string('description', 255)
+        .notNullable()
+
+        table
+        .boolean('completed')
+        .notNullable()
+
+        table
+        .timestamp('createdAt')
+        .defaultTo(knex.fn.now());
+    })
+
+    .createTable('actions', function(tbl) {
+        tbl.increments()
+
+        tbl
+        .string('description', 128)
+
+        tbl
+        .string('notes', 255)
+
+        tbl
+        .boolean('completed')
+        .notNullable()
+
+        tbl
+        .integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+
+        tbl
+        .timestamp('createdAt')
+        .defaultTo(knex.fn.now());
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('students');
+};
